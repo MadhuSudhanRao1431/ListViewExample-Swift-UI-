a09dd42c3c24083a62c783695374eacaef4b1010
madhu <madhu.sudhan@Connectyourneed.in>
2025-03-28 15:56:47 +0530

    Initial Commit

---
+++ ListViewExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListViewExample
+//
+//  Created by Madhu Sudhan on 28/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
